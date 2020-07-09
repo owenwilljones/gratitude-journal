90fcb1bcd5cdfe5b3403b36f6ebfc9ead9a60d68
owenatgov <64783893+owenatgov@users.noreply.github.com>
2020-07-09 09:44:51 +0100

    Add basic confirmation flow

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const submitGratitude = () => {
+  document.getElementById('submit-gratitude').addEventListener('click', (e) => {
+    const confContent = document.getElementById('confirmation-content');
+    const gratTextarea = document.getElementById('gratitudes-textarea');
+    const gratitudes = gratTextarea.value.split(/\r?\n/);
+    const confList = document.getElementById('confirmation-list');
+    const confButton = document.getElementById('confirm-gratitude');
+    
+    confContent.classList.remove('hidden');
+
+    for (let i = 0; i < gratitudes.length; i += 1) {
+      const li = document.createElement('li');
+      li.innerText = gratitudes[i]
+      confList.appendChild(li);
+    }
+
+    confButton.addEventListener('click', confirmAddGratitude);
+  });
+};
+
+const confirmAddGratitude = (event) => {
+  console.log('YEEEEEEEEAH');
+  document.getElementById('confirmation-content').classList.add('hidden');
+  document.getElementById('confirmation-list').textContent = '';
+
+  event.target.removeEventListener(event.type, confirmAddGratitude)
+};
+
 window.onload = () => {
-  
+  submitGratitude();
 }
