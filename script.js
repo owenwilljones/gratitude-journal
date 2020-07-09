f7ca5aefd36600c3d16b297c8951731295be07c6
owenatgov <64783893+owenatgov@users.noreply.github.com>
2020-07-09 15:19:45 +0100

    Establish cookie writing

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+const confContent = document.getElementById('confirmation-content');
+const gratTextarea = document.getElementById('gratitudes-textarea');
+const confList = document.getElementById('confirmation-list');
+const confButton = document.getElementById('confirm-gratitude');
+
 const submitGratitude = () => {
   document.getElementById('submit-gratitude').addEventListener('click', (e) => {
-    const confContent = document.getElementById('confirmation-content');
-    const gratTextarea = document.getElementById('gratitudes-textarea');
-    const confList = document.getElementById('confirmation-list');
-    const confButton = document.getElementById('confirm-gratitude');
-
     if (gratTextarea.value === '') {
       setErrorMessage('Please enter content into the textbox below');
       return;
@@ -28,11 +28,31 @@ const submitGratitude = () => {
 };
 
 const confirmAddGratitude = (event) => {
-  console.log('YEEEEEEEEAH');
-  document.getElementById('confirmation-content').classList.add('hidden');
-  document.getElementById('confirmation-list').textContent = '';
+  setCookie(gratTextarea.value.split(/\r?\n/));
 
-  event.target.removeEventListener(event.type, confirmAddGratitude)
+  confContent.classList.add('hidden');
+  confList.textContent = '';
+
+  confButton.removeEventListener(event.type, confirmAddGratitude)
+};
+
+const setCookie = (gratitudes) => {
+  const expiration = new Date();
+  const existingData = getGratitudeCookie();
+  const gratitudeData = existingData === undefined ? [] : JSON.parse(existingData);
+
+  gratitudeData.push({
+    timestamp: getTimestamp(),
+    gratitudes
+  });
+
+  expiration.setDate(expiration.getDate() + 7);
+  document.cookie = `gratitudes=${JSON.stringify(gratitudeData)}; expires=${expiration}`;
+};
+
+const getGratitudeCookie = () => {
+  const cookie = document.cookie.split('; ').find(cookie => cookie.indexOf('gratitudes') !== -1);
+  return cookie !== undefined ? cookie.split('=')[1] : cookie;
 };
 
 const setErrorMessage = (message) => {
@@ -45,8 +65,17 @@ const setErrorMessage = (message) => {
     errorMessage.classList.add('hidden');
     errorMessage.innerText = '';
   }
-}
+};
+
+const getTimestamp = () => {
+  const now = new Date();
+  return `${now.getDate()}/${timeFormatting(now.getMonth() + 1)}/${now.getFullYear()}, ${now.getHours()}:${timeFormatting(now.getMinutes())}`;
+};
+
+const timeFormatting = (num) => {
+  return num > 10 ? num.toString() : `0${num.toString()}`;
+};
 
 window.onload = () => {
   submitGratitude();
-}
+};
